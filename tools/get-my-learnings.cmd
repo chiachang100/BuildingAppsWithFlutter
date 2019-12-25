87958312128f9a0243c8e49ef7c02ed6bd23361a
Chia Chang <chiachang100@gmail.com>
2019-12-25 11:45:43 -0800

    Update get-my-learnings.cmd

--- tools/get-my-learnings.cmd
+++ tools/get-my-learnings.cmd
@@ -1,45 +1,6 @@
-mkdir MyLearnings
-pushd MyLearnings
-
 @echo off
-git clone https://github.com/chiachang100/BuildingBeautifulNativeAppsWithFlutter.git
-pushd BuildingBeautifulNativeAppsWithFlutter
+git clone https://github.com/chiachang100/FlutterKicks.git
+pushd FlutterKicks
 git pull
+popd
 @echo on
-popd
-
-@echo off
-git clone https://github.com/chiachang100/MasteringEthereumBlockchain.git
-pushd MasteringEthereumBlockchain
-git pull
-@echo on
-popd
-
-popd
-
-rem =======================================
-mkdir MyOtherLearnings
-pushd MyOtherLearnings
-
-@echo off
-git clone https://github.com/chiachang100/BuildingDeepLearningAppsWithTensorFlow.git
-pushd BuildingDeepLearningAppsWithTensorFlow
-git pull
-@echo on
-popd
-
-@echo off
-git clone https://github.com/chiachang100/BuildingBigDataAppsWithSpark.git
-pushd BuildingBigDataAppsWithSpark
-git pull
-@echo on
-popd
-
-@echo off
-git clone https://github.com/chiachang100/QuickPythonCourse.git
-pushd QuickPythonCourse
-git pull
-@echo on
-popd
-
-popd
